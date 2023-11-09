9e8d588f26d4a67bd276eb57d7a66d1ff9f4cb42
sylwestertylec <61873485+sylwestertylec@users.noreply.github.com>
2023-11-09 13:15:20 +0100

    Add files via upload

---
+++ War Card Game/ContentView.swift
@@ -0,0 +1,99 @@
+//
+//  ContentView.swift
+//  War Card Game
+//
+//  Created by Sylwester Tylec on 01/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+
+    @State var playerCard = "card7"
+    @State var cpuCard = "card12"
+    
+    @State var playerScore = 0
+    @State var cpuScore = 0
+    
+    
+    var body: some View {
+    
+    ZStack{
+        Image("background-plain")
+            .resizable()
+            .ignoresSafeArea()
+        
+        VStack{
+            Spacer()
+            Image("logo")
+            Spacer()
+            HStack{
+                Spacer()
+                Image(playerCard)
+                Spacer()
+                Image(cpuCard)
+                Spacer()
+            }
+            Spacer()
+            
+            
+            Button {
+                deal()
+            } label: {
+                Image("button")
+            }
+
+            
+            Spacer()
+        
+            HStack{
+                Spacer()
+                VStack{
+                Text("Player")
+                        .font(.headline)
+                        .padding(.bottom, 10.0)
+                    Text(String(playerScore))
+                        .font(.largeTitle)
+                        
+                }
+                Spacer()
+            
+                VStack{
+                    Text("CPU")
+                        .font(.headline)
+                        .padding(.bottom, 10.0)
+                    Text(String(cpuScore))
+                        .font(.largeTitle)
+                        
+                    
+                }
+                Spacer()
+            }
+            .foregroundColor(.white)
+            Spacer()
+        }
+    }
+}
+    
+    func deal(){
+        //Losowanie kard gracza
+        var playerCardValue = Int.random(in: 2...14)
+        playerCard = "card" + String(playerCardValue)
+        //Losowanie kard cpu
+        var cpuCardValue = Int.random(in: 2...14)
+        cpuCard = "card" + String(cpuCardValue)
+        //Aktualizacja punktów
+        if playerCardValue > cpuCardValue{
+            playerScore = playerScore + 1
+        }
+        else if cpuCardValue > playerCardValue{
+            cpuScore = cpuScore + 1
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Sylwester Tylec on 01/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
